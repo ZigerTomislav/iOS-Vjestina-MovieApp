95ca9fcd44ca084f4177d827f192379953afc51b
Tomislav Žiger <tz52709@fer.hr>
2022-03-29 11:40:45 +0200

    initialize project

---
+++ MovieApp/MovieDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailsViewController.swift
+//  MovieApp
+//
+//  Created by Tomislav Žiger  on 29.03.2022..
+//
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
